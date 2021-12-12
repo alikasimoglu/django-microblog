fbdc9a9220e128c9df1678c57c4a05a8f444d671
alikasimoglu <alikasimoglu@gmail.com>
2021-12-13 01:07:16 +0300

    Subscription algorithm created

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from profiles.views import ProfileListView
+from profiles.views import ProfileListView, ProfileDetailView, profile_subscribe_button
+
 
 app_name = 'profiles'
 urlpatterns = [
     path('', ProfileListView.as_view(), name='profiles'),
+    path('change_subscribe', profile_subscribe_button, name='profile_subscribe'),
+    path('<pk>/', ProfileDetailView.as_view(), name='profile-detail'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView
+from django.shortcuts import redirect
+from django.views.generic import ListView, DetailView
 from .models import Profile
 
 
@@ -9,3 +10,39 @@ class ProfileListView(ListView):
 
     def get_queryset(self):
         return Profile.objects.all().exclude(user=self.request.user)
+
+
+class ProfileDetailView(DetailView):
+    model = Profile
+    template_name = "profiles/profile_detail.html"
+    context_object_name = "profile"
+
+    def get_object(self, **kwargs):
+        pk = self.kwargs.get("pk")
+        view_profile = Profile.objects.get(pk=pk)
+        return view_profile
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        view_profile = self.get_object()
+        my_profile = Profile.objects.get(user=self.request.user)
+        if view_profile.user in my_profile.subscribed.all():
+            subscribe = True
+        else:
+            subscribe = False
+        context["subscribe"] = subscribe
+        return context
+
+
+def profile_subscribe_button(request):
+    if request.method == "POST":
+        my_profile = Profile.objects.get(user=request.user)
+        pk = request.POST.get("profile_pk")
+        obj = Profile.objects.get(pk=pk)
+
+        if obj.user in my_profile.subscribed.all():
+            my_profile.subscribed.remove(obj.user)
+        else:
+            my_profile.subscribed.add(obj.user)
+        return redirect(request.META.get("HTTP_REFERER"))
+    return redirect("profiles:profile-detail")
