bf2ced06fb16b13d0df3222c94a21b11950f8045
alikasimoglu <alikasimoglu@gmail.com>
2021-12-12 17:22:55 +0300

    Modules created

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
+from profiles.models import Profile
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    post_title = models.CharField("Post Title", max_length=150, unique=True)
+    post_content = models.TextField("Content")
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.post_title
+
+    class Meta:
+        ordering = ("-created",)
--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    subscribed = models.ManyToManyField(User, related_name='subscribed', blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def profile_blog_posts(self):
+        return self.blogpost_set.all()
+
+    def __str__(self):
+        return str(self.user.username)
+
+    class Meta:
+        ordering = ("-created",)
